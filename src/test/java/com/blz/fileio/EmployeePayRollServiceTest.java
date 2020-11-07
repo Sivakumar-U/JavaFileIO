2ef1638fa3b2490de2a858cec4b4cca14ac5dbbe
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-07 19:40:48 +0530

    Added: Test case to create an employee payroll service to store employee payroll into a file

---
+++ src/test/java/com/blz/fileio/EmployeePayRollServiceTest.java
@@ -0,0 +1,18 @@
+package com.blz.fileio;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class EmployeePayRollServiceTest {
+
+	@Test
+	public void givenTwoEmloyeesDetails_WrittenToFile_ShouldMatchWithEntries() {
+		EmployeePayrollData[] arrayofEmps = { new EmployeePayrollData(1, "James", 25000.0),
+				new EmployeePayrollData(2, "Mickey", 50000.0) };
+		EmployeePayrollService empPayrollService;
+		empPayrollService = new EmployeePayrollService(Arrays.asList(arrayofEmps));
+		empPayrollService.writeEmployeePayrollData(com.blz.fileio.EmployeePayrollService.IOService.FILE_IO);
+	}
+
+}
